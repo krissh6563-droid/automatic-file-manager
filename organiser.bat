8c58ff8d8c3b702da8aaa80cee29c8ebf3bcb5e6
krissh6563-droid <krishnamitws2000@gmail.com>
2021-05-30 13:12:57 +0530

    Error Solve

--- organiser.bat
+++ organiser.bat
@@ -1,5 +1,5 @@
 @echo off
 
-python C:\Users\Shri Krishan\Desktop\move_file_automatic\app.py
+"C:\Users\Shri Krishan\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\Shri Krishan\Desktop\move_file_automatic\app.py"
 
 @pause
